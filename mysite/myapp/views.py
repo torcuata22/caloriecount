a200e6a66a4dc4bc6c6a670e2856100fe561e117
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-11-14 15:27:20 -0600

    bootstrap

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
-from .models import Food
+from .models import Consume, Food
+from django.contrib.auth.models import User
 # Create your views here.
 
 def index(request):
-    foods = Food.objects.all()
-    return render(request, "myapp/index.html",{"foods":foods})
+
+    if request.method == "POST":
+        food_consumed = request.POST['food_consumed'] #name alone, from the form
+        consume = Food.objects.get(name=food_consumed) # gets the object that matches the name "food_consumed"
+        #get current user:
+        user = request.user
+        #construct object of type Consume:
+        consume = Consume(user = user, food_consumed=consume)
+        consume.save()
+        foods = Food.objects.all()
+
+
+    else:
+        foods = Food.objects.all()
+    consumed_food = Consume.objects.filter(user=request.user) #fetches all consumed items
+    return render(request, "myapp/index.html",{"foods":foods, 'consumed_food':consumed_food})
